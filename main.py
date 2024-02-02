97171f4bd2503e7732d65aaf16314c007a30288c
Luis Gabriel <luisgabrielprieto@gmail.com>
2024-02-02 10:42:45 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import turtle as t
 from snake import Snake
+import time
 
 screen = t.Screen()
 screen.bgcolor("black")
 screen.screensize(canvwidth=600,canvheight=600)
+screen.listen()
 
 snake = Snake()
-# snake.snake_body()
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(1)
+    snake.move_snake()
+
+
 
 
 
--- snake.py
+++ snake.py
@@ -1,25 +1,49 @@
 import turtle as t
 
-INITIAl_SIZE = 3
-
+INITIAL_SIZE = 3
 
 class Snake(t.Turtle):
 
     def __init__(self):
         super().__init__()
         self.snake_parts = []
+        self.snake_positions = []
         self.snake_body()
+        self.snake_head = self.snake_parts[0]
 
     def snake_body(self):
 
-        for i in range(INITIAl_SIZE):
-            new_turtle = Snake()
-            new_turtle.penup()
+        for i in range(INITIAL_SIZE):
+            new_turtle = t.Turtle()
             new_turtle.shape("square")
             new_turtle.color("white")
-            new_turtle.goto(x = 20*i, y = 0)
+            new_turtle.penup()
+            new_turtle.goto(x=-20*i, y=0)
             self.snake_parts.append(new_turtle)
 
+    def move_snake(self):
 
-        # self.goto(x=-20*i, y=0)
-        # self.snake_parts.append(new_turtle)
+        for part in self.snake_parts:
+            position = part.position()
+            self.snake_positions.append(position)
+
+        print(self.snake_positions)
+
+        for i in range(1,len(self.snake_parts)):
+
+            self.snake_parts[i].goto(self.snake_positions[i-1])
+
+        self.snake_head.forward(20)
+        self.snake_head.setheading(90)
+
+        self.snake_positions = []
+
+    # def move_right(self):
+
+
+
+
+
+
+
+
